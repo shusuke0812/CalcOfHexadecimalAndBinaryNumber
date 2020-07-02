40a88854b46dfa9842d88ad821ed828c4ce6ef7e
shusuke0812 <shu.ota0812@gmail.com>
2020-07-03 07:47:06 +0900

    [add]変数定義

---
+++ js_app/app.js
@@ -0,0 +1,15 @@
+// 変数定義
+let inputBinChange = document.getElementById("js-inputBinChange").value;
+let inputDecChange = document.getElementById("js-inputDecChange").value;
+let inputHexChange = document.getElementById("js-inputHexChange").value;
+
+let inputCalc = []
+let inputBinCalc = document.getElementById("js-inputBinCalc").value
+let inputHexCalc = document.getElementById("js-inputHexCalc").value
+
+const tapBinBtn = docunemt.getElementById("js-tapBinBtn")
+const tapDecBtn = docunemt.getElementById("js-tapDecBtn")
+const tapHexBtn = docunemt.getElementById("js-tapHexBtn")
+
+// 変換
+

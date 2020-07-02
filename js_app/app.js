467206f847906bebc2bf9eb938d5f013bed219a5
shusuke0812 <shu.ota0812@gmail.com>
2020-07-03 08:57:19 +0900

    [add]基数変換機能を実装

--- js_app/app.js
+++ js_app/app.js
@@ -1,15 +1,54 @@
 // 変数定義
-let inputBinChange = document.getElementById("js-inputBinChange").value;
-let inputDecChange = document.getElementById("js-inputDecChange").value;
-let inputHexChange = document.getElementById("js-inputHexChange").value;
+var inputBin = 0
+var inputDec = 0
+var inputHex = 0
 
-let inputCalc = []
-let inputBinCalc = document.getElementById("js-inputBinCalc").value
-let inputHexCalc = document.getElementById("js-inputHexCalc").value
+//let inputCalc = []
+//let inputBinCalc = document.getElementById("js-inputBinCalc").value
+//let inputHexCalc = document.getElementById("js-inputHexCalc").value
 
-const tapBinBtn = docunemt.getElementById("js-tapBinBtn")
-const tapDecBtn = docunemt.getElementById("js-tapDecBtn")
-const tapHexBtn = docunemt.getElementById("js-tapHexBtn")
+const tapBinBtn = document.getElementById("js-tapBinBtn")
+const tapDecBtn = document.getElementById("js-tapDecBtn")
+const tapHexBtn = document.getElementById("js-tapHexBtn")
 
-// 変換
+// 変換 
+// TODO:エラーハンドリング
+// 2進数 -> 10進数・16進数
+tapBinBtn.addEventListener('click', () => {
+	inputBin = document.getElementById("js-inputBin").value;
+	if (inputBin) {
+		inputDec = parseInt(inputBin, 2);
+		inputHex = inputDec.toString(16);
+		document.getElementById("js-inputDec").value = inputDec;
+		document.getElementById("js-inputHex").value = inputHex;
+	}
+	console.log("[DEBUG]Bin->Dec: ", inputDec);
+	console.log("[DEBUG]Bin->Hex: ", inputHex);
+})
 
+// 10進数 -> 2進数・16進数
+tapDecBtn.addEventListener('click', () => {
+	inputDec = document.getElementById("js-inputDec").value;
+	if (inputDec) {
+		var ans = parseInt(inputDec);
+		inputBin = ans.toString(2);
+		inputHex = ans.toString(16);
+		document.getElementById("js-inputBin").value = inputBin;
+		document.getElementById("js-inputHex").value = inputHex;
+	}
+	console.log("[DEBUG]Dec->Bin: ", inputBin);
+	console.log("[DEBUG]Dec->Hex: ", inputHex);
+})
+
+// 16進数 -> 2進数・10進数
+tapHexBtn.addEventListener('click', () => {
+	inputHex = document.getElementById("js-inputHex").value;
+	if (inputHex) {
+		inputDec = parseInt(inputHex, 16);
+		inputBin = inputDec.toString(2);
+		document.getElementById("js-inputDec").value = inputDec;
+		document.getElementById("js-inputBin").value = inputBin;
+	}
+	console.log("[DEBUG]Hex->Dec: ", inputDec);
+	console.log("[DEBUG]Hex->Bin: ", inputBin)
+})
